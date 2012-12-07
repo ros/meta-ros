eb9318bb9c6db7ca1d9fd5b2cb30bad2f529d822
Lukas Bulwahn <lukas.bulwahn@bmw-carit.de>
2012-12-07 14:09:41 +0100

    extending installation script so that it patches, compiles, installs and starts ROS; extending README

--- install.sh
+++ install.sh
@@ -10,8 +10,40 @@ rosinstall --catkin ~/ros fuerte-ros-base.rosinstall
 git clone ssh://lukas@192.168.7.1//home/lukas/ros_server/rospkg
 cd ~/rospkg
 python setup.py install
+# apply patch
+cd ~/ros/ros_comm
+patch -p1 << EOF
+diff -cr ros_comm/tools/rosbag/src/recorder.cpp ros_comm.patched/tools/rosbag/src/recorder.cpp
+*** ros_comm/tools/rosbag/src/recorder.cpp	2012-12-06 14:12:06.773910947 +0100
+--- ros_comm.patched/tools/rosbag/src/recorder.cpp	2012-12-06 14:14:49.145917680 +0100
+***************
+*** 436,442 ****
+                  break;
+              }
+              boost::xtime xt;
+!             boost::xtime_get(&xt, boost::TIME_UTC);
+              xt.nsec += 250000000;
+              queue_condition_.timed_wait(lock, xt);
+              if (checkDuration(ros::Time::now()))
+--- 436,442 ----
+                  break;
+              }
+              boost::xtime xt;
+!             boost::xtime_get(&xt, boost::TIME_UTC_);
+              xt.nsec += 250000000;
+              queue_condition_.timed_wait(lock, xt);
+              if (checkDuration(ros::Time::now()))
+EOF
 # cmake invocation
 cd ~/ros
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=/opt/ros/fuerte -DSETUPTOOLS_DEB_LAYOUT=OFF
+# compile and install
+make
+make install
+# add own name to hosts
+echo -e '127.0.0.1\tqemux86-64.localdomain\t\tqemux86-64' >> /etc/hosts
+# start roscore
+. /opt/ros/fuerte/setup.sh
+roscore
