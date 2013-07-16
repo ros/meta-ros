c87faa6db91102c181bcd128536482df95fd8dd0
Lukas Bulwahn <lukas.bulwahn@oss.bmw-carit.de>
2013-07-16 07:48:02 +0200

    test-all.sh: tuned and commented out fresh build
    The bitbake call before each build of a recipe was not required.
    Commented out the fresh build, as it currently does not show more
    problems than simply building in a shared environment.
    Avoiding a fresh environment for each build decreases the runtime
    of the test dramatically.

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -136,8 +136,9 @@ bake_all () {
 	activity="$txt_file_pkg_checking_kw $((i+1))/$array_length: ${array[$i]}"
 	echo $activity
         # create clean bitbake environment
-	rm bitbake.lock cache/ sstate-cache/ tmp-eglibc/ -rf
-        bitbake
+	# uncomment the following line if it should always clean the environment
+        # for each bitbake run
+        # rm bitbake.lock cache/ sstate-cache/ tmp-eglibc/ -rf
 	if [ "$arg" = "txt" ]; then
 	    # Try to bake every recipe. Store results in a report file. Just give $txt_file_pkg_ok_kw for working recipes, print more information on errors.
 	    echo $activity | tee -a "$report_file_raw" >> "$report_file_txt"
