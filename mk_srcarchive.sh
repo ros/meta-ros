e5c6b214920df097be87136530e4c0c6c68284c8
Lukas Bulwahn <lukas.bulwahn@oss.bmw-carit.de>
2013-07-22 13:12:41 +0200

    removing now obsolete files for previous on-target compilation

--- mk_srcarchive.sh
+++
@@ -1,5 +0,0 @@
-wget -O ros_comm http://packages.ros.org/web/rosinstall/generate/raw/groovy/ros_comm
-mkdir -p archives
-grep -e "- tar: {local-name: .*, uri: '.*'," ros_comm | sed "s#- tar: {local-name: \(.*\), uri: '\(.*\)',#wget -O archives/\1.tar.gz \2 \&\& mkdir -p src/\1 \&\& tar --strip-components=1 -xzf archives/\1.tar.gz -C src/\1/ #" | sh
-tar -czf src.tar.gz src
-
