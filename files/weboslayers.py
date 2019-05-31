6c26ee70fdf8d81df390f849e7437895e32cd5a6
Herb Kuta <herb.kuta@lge.com>
2019-05-31 09:43:09 -0700

    weboslayers.py: Convert to file format version 2, must use mcf 7.0.0 or later
    - Add McfFileVersion.
    - fetch, options => dict-s
    - webos_layers -> Layers
    - Add BblayersConfExtraLines.

--- files/weboslayers.py
+++ files/weboslayers.py
@@ -12,40 +12,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+McfFileVersion = 2
+
 # Value for DISTRO
-Distribution = "webos"
+Distribution = 'webos'
 
 # Supported MACHINE-s
-Machines = ['raspberrypi3','qemux86']
+Machines = ['qemux86', 'raspberrypi3']
 
-# webos_layers = [
-# ('layer-name', priority, 'URL', 'submission', 'working-dir'),
+# Layers = [
+# (layer-name: str, priority: int, URL: str, fetch: dict, options: dict),
 # ...
 # ]
 # Note that the github.com/openembedded repositories are read-only mirrors of
 # the authoritative repositories on git.openembedded.org .
-webos_layers = [
-('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.40,commit=7c1eb51d', ''),
+Layers = [
+('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              {'branch': '1.40', 'commit': '7c1eb51d'}, {}),
 
-('meta-gplv2',                 1, 'git://git.yoctoproject.org/meta-gplv2',                  'branch=thud,commit=aabc30f', ''),
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=thud,commit=f162d5bfe6', ''),
+('meta-gplv2',                 1, 'git://git.yoctoproject.org/meta-gplv2',                  {'branch': 'thud', 'commit': 'aabc30f'}, {}),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    {'branch': 'thud', 'commit': 'f162d5bfe6'}, {}),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=thud,commit=9b3b907f30', ''),
-('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', ''),
-('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', ''),
-('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', ''),
-('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', ''),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    {'branch': 'thud', 'commit': '9b3b907f30'}, {}),
+('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    {}, {}),
+('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    {}, {}),
+('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    {}, {}),
+('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    {}, {}),
 
-('meta-qt5-compat',           19, 'git://github.com/webosose/meta-webosose.git',            '', ''),
-('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=krogoth,commit=dcfcb58', ''),
+('meta-qt5-compat',           19, 'git://github.com/webosose/meta-webosose.git',            {}, {}),
+('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 {'branch': 'krogoth', 'commit': 'dcfcb58'}, {}),
 
-('meta-webos-backports-2.7',  30, 'git://github.com/webosose/meta-webosose.git',            '', ''),
-('meta-webos-backports-2.8',  31, 'git://github.com/webosose/meta-webosose.git',            '', ''),
+('meta-webos-backports-2.7',  32, 'git://github.com/webosose/meta-webosose.git',            {}, {}),
+('meta-webos-backports-2.8',  33, 'git://github.com/webosose/meta-webosose.git',            {}, {}),
 
-('meta-ros2',                 38, 'git://github.com/lgsvl/meta-ros2.git',                   'branch=master,commit=577f371',''),
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=4e3ddd5', ''),
+('meta-ros2',                 38, 'git://github.com/lgsvl/meta-ros2.git',                   {'branch': 'master', 'commit': '577f371'}, {}),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            {'branch': 'master', 'commit': '4e3ddd5'}, {}),
 
-('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=thud,commit=c71d79e', ''),
-('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
-('meta-webos-ros2',           52, 'git://github.com/webosose/meta-webosose.git',            '', ''),
+('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            {'branch': 'thud', 'commit': 'c71d79e'}, {}),
+('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            {}, {}),
+
+('meta-webos-ros2',           52, 'git://github.com/webosose/meta-webosose.git',            {}, {}),
 ]
+
+# BblayersConfExtraLines is a list of strings to be appended to the generated
+# conf/bblayers.conf .
+BblayersConfExtraLines = []
