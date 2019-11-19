db41490c5de633d3029cc347c645d9b31707347e
Martin Jansa <martin.jansa@lge.com>
2019-11-19 13:01:42 -0800

    ros-generate-cache: update cache.diffme as well (v1.1.0)
    Signed-off-by: Martin Jansa <martin.jansa@lge.com>

--- scripts/ros-generate-cache.sh
+++ scripts/ros-generate-cache.sh
@@ -18,7 +18,7 @@
 # Copyright (c) 2019 LG Electronics, Inc.
 
 readonly SCRIPT_NAME="ros-generate-cache"
-readonly SCRIPT_VERSION="1.0.0"
+readonly SCRIPT_VERSION="1.1.0"
 
 # Files under ros/rosdistro/rosdep that we care about. Keep in sync with setting in ros-generate-recipes.sh .
 readonly ROSDEP_YAML_BASENAMES="base python ruby"
@@ -138,6 +138,10 @@ mkdir -p $generated
 gzip -d $tmpdir/$ROS_DISTRO-cache.yaml.gz -c > $generated/cache.yaml
 # Identify how the files were generated so that they can be reused.
 sed -i -e "1 i# $ROS_DISTRO/cache.yaml $ROS_DISTRO_RELEASE_DATE $ROS_ROSDISTRO_COMMIT $ROS_ROSDISTRO_COMMIT_DATETIME" files/$ROS_DISTRO/cache.yaml
+sed -e 's@{\([^ }][^ }]*\)}@[[\1]]@g' -e 's@{@{\n@g' -e 's@}@\n}@g' -e 's@\[\[@{@g' -e 's@]]@}@g' -e 's@, @,\n@g' \
+    -e 's@^    @-----\n@' -e 's@<version>[^<]*</version>@@' -e 's@><@>\n<@g' -e 's@^  @-----\n@' \
+    -e 's@^\(source_repo_package_xmls:\)@-----\n\1@' \
+    files/$ROS_DISTRO/cache.yaml > files/$ROS_DISTRO/cache.diffme
 
 mkdir -p $generated/rosdep
 cp -f $tmpdir/index-v4.yaml $generated/
