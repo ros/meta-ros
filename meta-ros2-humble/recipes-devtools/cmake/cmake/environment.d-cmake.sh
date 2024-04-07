94bdefcf654dd6fda71836de22abf7cff6ed3e26
Rob Woolley <rob.woolley@windriver.com>
2024-04-07 15:40:38 -0700

    {common}{humble}{iron}{rolling} cmake: Remove recipe
    Copies of the cmake recipe were added for older Yocto Project
    releases that did not meet the minimum version requirement.
    This is no longer required by any supported releases and the
    one provided by OpenEmbedded is newer.
    
    Also remove the cmake bbappend in meta-ros-common.  It was
    added originally to prevent the toolchain file for the SDK
    from being placed in nativesdk-cmake-dev.  However, this
    no longer happens and the change is no longer required.
    
    Signed-off-by: Matthias Schoepfer <matthias.schoepfer@googlemail.com>
    Signed-off-by: Rob Woolley <rob.woolley@windriver.com>

--- meta-ros2-humble/recipes-devtools/cmake/cmake/environment.d-cmake.sh
+++
@@ -1,2 +0,0 @@
-export OE_CMAKE_TOOLCHAIN_FILE="$OECORE_NATIVE_SYSROOT/usr/share/cmake/OEToolchainConfig.cmake"
-export OE_CMAKE_FIND_LIBRARY_CUSTOM_LIB_SUFFIX="`echo $OECORE_BASELIB | sed -e s/lib//`"
